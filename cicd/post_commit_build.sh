6c95807201dc9bb144ad50e0fff2f4a3e9d3238e
Gavin R. Barnard <gavin.r.barnard@gmail.com>
2021-04-15 00:11:20 -0400

    cicd: post commit build release and debug (#4)
    add the following to .git/hooks/post-commit
    and then chmod +x .git/hooks/post-commit
    ```
    #!/bin/sh
    
    checkout=`git log -1 HEAD | head -1 | awk '{print $2}'`
    basedir=`git rev-parse --show-toplevel`
    
    exec $basedir/cicd/post_commit_build.sh $checkout local
    ```

---
+++ cicd/post_commit_build.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#THIS SCRIPT ASSUMES MONERO_ROOT IS SET#####
+
+checkout=$1
+mode=$2
+repo=$3
+TMP=/tmp/
+OWD=`pwd`
+
+
+if [[ -z "$checkout" ]]; then
+	echo "Failure no checkout id";
+	exit 1;
+fi
+
+if [[ -z "$mode" ]]; then
+	echo "no mode";
+	exit 1;
+fi
+if [[ "$mode" == "remote" ]]; then
+	if [[ -z "$repo" ]]; then
+		echo "remote mode missing repo";
+		exit 1;
+	fi
+	git_root=$3;
+fi
+
+if [[ "$mode" == "local" ]]; then
+	git_root=`git rev-parse --show-toplevel`;
+fi
+
+
+project_name=`echo $git_root | awk -F '/' '{print $NF}'`
+TMP=$TMP"`date --iso-8601=seconds`-"$project_name
+mkdir -p $TMP
+cd $TMP
+git clone $git_root
+cd $project_name
+git checkout $checkout
+make release
+release=$?
+
+rm -rf build
+make
+debug=$?
+
+cd $OWD
+rm -rf $TMP
+
+if [[ $debug -eq 0 ]]  && [[ $release -eq 0 ]]; then
+	exit 0;
+else
+
+	echo "# Build failure fix the build failure in debug:$debug or release:$release non-zero is fail"
+	echo "# reset to the previous commit with the following command"
+	echo "# and update your code"
+	echo " git reset --soft HEAD~1"
+	exit 1;
+fi
+
+
